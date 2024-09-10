9e2bd4c66aeb4917949769087b32a629cc50cf4a
its-harshitgoel <its.harshitgoel@gmail.com>
2024-09-10 14:40:53 +0530

    camelCase cf-800

---
+++ Questions/String/camelCase.java
@@ -0,0 +1,23 @@
+package Questions.String;
+import java.util.Scanner;
+
+public class camelCase {
+    //determine the number of words
+    public static int camelCase(String s) {
+        int count = 1;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if(ch >= 'A' && ch <= 'Z'){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(camelCase(s));
+        sc.close();
+    }
+}
